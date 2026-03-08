c05a81cdd760b7bbc2f5b14af65ebe7da73e09c0
pedrohenriquebraga <pedrobraga693@gmail.com>
2026-03-08 12:39:08 -0300

    add input validation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import Message from "@/components/Message";
 import { Feather } from "@expo/vector-icons";
-import React from "react";
+import React, { useState } from "react";
 import {
   ScrollView,
   StyleSheet,
@@ -10,19 +10,24 @@ import {
 } from "react-native";
 
 const MessagesScreen: React.FC = () => {
+  const [inputMessage, setInputMessage] = useState("");
+
   return (
     <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.message_container}>
         <Message content="Teste mensagem ao lado direito" isRight />
         <Message content="Teste mensagem ao lado esquerdo" />
+        <Message content="Teste mensagem ao lado direito 2" isRight />
       </ScrollView>
       <View style={styles.input_container}>
         <TextInput
           style={styles.message_input}
           placeholderTextColor={"#8E8E93"}
           placeholder="Digite sua mensagem..."
+          value={inputMessage}
+          onChangeText={setInputMessage}
         />
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} disabled={!inputMessage.length}>
           <Feather
             name="send"
             size={20}
